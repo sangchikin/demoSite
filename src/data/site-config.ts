aa1efa71d74d0624dd2f10807fcad9e9d3852fb4
sangchikin <sangchikin@gmail.com>
2025-12-19 19:28:03 +0700

    Update website URL to include HTTPS

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -3,7 +3,7 @@ import hero from '../assets/images/hero.jpg';
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
-    website: 'demosite-aw4.pages.dev',
+    website: 'https://demosite-aw4.pages.dev/',
     avatar: {
         src: avatar,
         alt: 'Ethan Donovan'
