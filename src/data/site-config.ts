355a2fd3d3607a2cc4d86d5b517fd232f95437f2
sangchikin <sangchikin@gmail.com>
2025-12-19 16:07:29 +0700

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -3,7 +3,7 @@ import hero from '../assets/images/hero.jpg';
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
-    website: 'https://changiau.com',
+    website: 'demosite-aw4.pages.dev',
     avatar: {
         src: avatar,
         alt: 'Ethan Donovan'
